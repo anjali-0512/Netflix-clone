e40551b47320b90f56b050b1f024ff8ef069be46
Anjali Prasoon <anjaliprasoon@Anjalis-MacBook-Pro.local>
2021-10-20 22:35:54 +0530

    Adding the Row file

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import instance from './axios';
 
-function Row({title}){
+function Row({title, fetchUrl}){
+
+    const baseURL= "https://www.themoviedb.org/t/p/original/";
+    const [movies, setMovies]=useState([]);
+
+    useEffect(()=>{
+
+        async function fetchData(){
+            const request = await instance.get(fetchUrl);
+            //console.log(request);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData();
+        
+    }, [fetchUrl]);
+
+    
+    
     return(
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+            <div className="row_posters">
+                {movies.map((movie)=>(
+                    <img src={`${baseURL}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            
+
+            </div>
+            
         </div>
     )
 }
