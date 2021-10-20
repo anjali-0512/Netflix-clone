75875ed7c28905476bb72027567f4243a676f86f
Anjali Prasoon <anjaliprasoon@Anjalis-MacBook-Pro.local>
2021-10-20 22:37:12 +0530

    API call and displaying the photos for different categories

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 //base url to make request to movie database
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
--- src/requests.js
+++ src/requests.js
@@ -3,7 +3,7 @@ const API_KEY = "47b6418ef9d402256029c3c1ce1b3ac3";
 const requests={
 
     fetchtrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `discover/tv?api_key=${Api_KEY}&with_networks=123`,
+    fetchNetflixOriginals: `discover/tv?api_key=${API_KEY}&with_networks=123`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
